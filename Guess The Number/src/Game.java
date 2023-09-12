598bc80f6abbd3c0728e01080fbde3cda3b2a2a6
Naveen Kumar <connect.naveen@gmail.com>
2023-09-12 22:58:42 +0530

    some major changes. added play again feature. ;) added score feature.
    done with the project.

--- Guess The Number/src/Game.java
+++ Guess The Number/src/Game.java
@@ -4,6 +4,9 @@ public class Game {
     private int input;
     private int counter=0;
 
+    private static int count1 = 0;
+    private static int count2 = 0;
+
     public Game(){
         // random no generator from 0 to 100
         this.random = (int)(Math.random() * 100);
@@ -16,6 +19,8 @@ public class Game {
     public int isCorrect(int input){
         if(input == random){
             System.out.println("Yoo-hoo! You Guessed it right.");
+            count2++;
+
             return 1;
         }
         if(input<random){
@@ -32,33 +37,54 @@ public class Game {
 
     public static void main(String[] args) {
 
-        Game ob = new Game();
-        System.out.println("Let's a game of guessing the numbers;");
+        System.out.println("Let's play a game of guessing the numbers;");
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the no of guesses: ");
-        ob.counter = in.nextInt();
-
-        // this prints the random generated no.
-       // System.out.println(ob.random);
 
 
+        //   First we create two integers to count the number of plays to find the final score.
 
-        while (ob.counter!=0){
+        // another integer to ask whether the user wants to play again or not.
+        int playagain;
+        // this is the main loop which runs until the user wants to play again.
+        do {
+            Game ob = new Game();
+            count1++;
 
-            System.out.print("Enter the no:");
-            int n = in.nextInt();
+            System.out.print("Enter the no of guesses: ");
+            ob.counter = in.nextInt();
 
-            ob.inputno(n);
+            // this prints the random generated no.
+            // System.out.println(ob.random);
 
-            int x = ob.isCorrect(ob.input);
-            if(x==1){
-                break;
+
+            while (ob.counter != 0) {
+
+                System.out.print("Enter the no:");
+                int n = in.nextInt();
+
+                ob.inputno(n);
+
+                int x = ob.isCorrect(ob.input);
+                if (x == 1) {
+                    break;
+                }
+                ob.counter--;
             }
-            ob.counter--;
-        }
 
+            System.out.println("Want to play again?");
+            System.out.println("Press 1 for Yes and 0 for No");
+            playagain = in.nextInt();
+        }while(playagain==1);
 
+        System.out.println("Your Final Score is: " + count1 + " out of " + count2 + " games played.");
+        System.out.println("Thank You for playing the game. Have a nice day :)");
+        System.out.println("Bye Bye!!");
+        System.out.println("Created by: Naveen Kumar");
+        System.out.println("Github: https://github.com/itsnaveenk/CODSOFT.git");
+        System.out.println("LinkedIn: https://www.linkedin.com/in/itsnaveenk/");
+        System.out.println("Twitter: https://twitter.com/itsnaveenk");
 
+        in.close();
 
     }
 }
